f3940d94297bc02a9520462d8066f7f9c9f51da3
evergreen <tkdfhr7497@naver.com>
2025-01-01 15:48:25 +0900

    [Chore] jvm version 1.8

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,15 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.android.library) apply false
+}
+
+allprojects {
+    tasks.withType<KotlinJvmCompile>().configureEach {
+        compilerOptions.jvmTarget = JvmTarget.JVM_1_8
+    }
 }
