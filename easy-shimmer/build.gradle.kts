f0cab116e00dbb7e74415ec31fd120b3c22b9b69
evergreen <tkdfhr7497@naver.com>
2025-01-04 02:28:06 +0900

    [Release] 0.0.1 setting

--- easy-shimmer/build.gradle.kts
+++ easy-shimmer/build.gradle.kts
@@ -1,7 +1,11 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    id("maven-publish")
 }
+
+group = "com.github.EvergreenTree97"
+version = "0.0.1"
 
 android {
     namespace = "io.github.easyshimmer"
@@ -20,8 +24,25 @@ android {
     composeOptions {
         kotlinCompilerExtensionVersion = libs.versions.composeCompiler.get()
     }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
 }
 
 dependencies {
     implementation(libs.coil.compose)
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("maven") {
+                groupId = "com.github.EvergreenTree97"
+                artifactId = "easy-shimmer-compose"
+                version = "0.0.1"
+
+                from(components["release"])
+            }
+        }
+    }
 }
